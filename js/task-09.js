6ecc3bd3d223a29ea207947c7faa161e14000416
Olehys <arzamazova13@gmail.com>
2022-10-30 18:35:46 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyBgColor = document.querySelector("body");
+const resetBtnColor = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+const resetBodyColor = () => {
+  bodyBgColor.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+};
+
+resetBtnColor.addEventListener("click", resetBodyColor);
